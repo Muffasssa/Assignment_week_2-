e8f2dd22603aa4a43c4b9aa2145cdd33fb47dff9
Muffassa <mervynstunner@gmail.com>
2019-03-20 10:21:34 +0300

    minor changes

---
+++ null_checker.js
@@ -0,0 +1,11 @@
+//var x = 0;
+const nullChecker = num => {
+  if (num > 0){
+    console.log ("1");
+  }else if (num < 0){
+    console.log ("-1");
+  }else if (num ===0){
+    console.log ("0");
+  }
+}
+console.log(nullChecker(0));
